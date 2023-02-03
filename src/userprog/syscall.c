fcc87149649995735417ac23187eb8fdd2a5d510
Abe Yosef <ayosef@myth59.stanford.edu>
2023-02-02 19:11:11 -0800

    things print but still a page fault

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -25,10 +25,10 @@ static void sys_close (uint32_t *esp);
 
 static void exit (int status);
 
-static void *get_arg_buffer (uint32_t *esp, int pos, int size);
-static int get_arg_int (uint32_t *esp, int pos);
+static void *get_arg_buffer (void *esp, int pos, int size);
+static int get_arg_int (void *esp, int pos);
 static bool is_valid_address (const void *uaddr);
-static bool is_valid_buffer (void *buffer, unsigned size);
+static bool is_valid_memory (void *buffer, unsigned size);
 
 static struct lock filesystem_lock;
 
@@ -40,69 +40,71 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  
-  uint32_t *esp;
   uint32_t syscall_num;
-  
-  esp = f->esp;
-  syscall_num = get_arg_int (esp, 0);
 
+  if (!is_valid_address (f->esp))
+    exit (-1);
+
+  syscall_num = get_arg_int(f->esp, 0);
+  printf("syscall_num = %d\n", syscall_num);
   switch (syscall_num)
   {
     case SYS_HALT:                   /* Halt the operating system. */
-      printf ("%s", "halt");
+      // printf ("%s\n", "about to halt");
       sys_halt ();
       break;
     case SYS_EXIT:                   /* Terminate this process. */
-      printf("%s", "exit");
-      sys_exit (esp);
+      // printf("%s\n", "about to exit");
+      sys_exit (f->esp);
       break;
     case SYS_EXEC:                   /* Start another process. */
-      printf("%s", "exec");
-      sys_exec (esp);
+      // printf("%s\n", "about to exec");
+      sys_exec (f->esp);
       break;
     case SYS_WAIT:                   /* Wait for a child process to die. */
-      printf("%s", "wait");
-      sys_wait (esp);
+      // printf("%s\n", "about to wait");
+      sys_wait (f->esp);
       break;
     case SYS_CREATE:                 /* Create a file. */
-      printf("%s", "create");
-      sys_create (esp);
+      // printf("%s\n", "about to create");
+      sys_create (f->esp);
       break;
     case SYS_REMOVE:                 /* Delete a file. */
-      printf("%s", "remove");
-      sys_remove (esp);
+      // printf("%s\n", "about to remove");
+      sys_remove (f->esp);
       break;
     case SYS_OPEN:                   /* Open a file. */
-      printf("%s", "open");
-      sys_open (esp);
+      // printf("%s\n", "about to open");
+      sys_open (f->esp);
       break;
     case SYS_FILESIZE:               /* Obtain a file's size. */
-      printf("%s", "filesize");
-      sys_filesize (esp);
+      // printf("%s\n", "about to filesize");
+      sys_filesize (f->esp);
       break;
     case SYS_READ:                   /* Read from a file. */
-      printf("%s", "read");
-      sys_read (esp);
+      // printf("%s\n", "about to read");
+      sys_read (f->esp);
       break;
     case SYS_WRITE:                  /* Write to a file. */
-      printf("%s", "write");
-      f->eax = sys_write (esp);
+      // printf("%s\n", "about to write");
+      f->eax = sys_write (f->esp);
       break;
     case SYS_SEEK:                   /* Change position in a file. */
-      printf("%s", "seek");
-      sys_seek (esp);
+      // printf("%s\n", "about to seek");
+      sys_seek (f->esp);
       break;
     case SYS_TELL:                   /* Report current position in a file. */
-      printf("%s", "tell");
-      sys_tell (esp);
+      // printf("%s\n", "about to tell");
+      sys_tell (f->esp);
       break;
     case SYS_CLOSE:                  /* Close a file. */
-      printf("%s", "close");
-      sys_close (esp);
+      // printf("%s\n", "about to close");
+      sys_close (f->esp);
       break;
+    default:
+      exit(-1000);
   }
 }
 
@@ -123,12 +125,12 @@ sys_halt ()
 }
 
 void
-sys_exit (uint32_t *esp UNUSED)
+sys_exit (uint32_t *esp)
 {
   int status;
-
   status = get_arg_int (esp, 1);
-  exit(status);
+  printf("status is %d\n", status);
+  exit (status);
 }
 
 pid_t
@@ -177,13 +179,16 @@ int
 sys_write (uint32_t *esp)
 {
   int fd;
-  const void *buffer;
+  char *buffer;
   unsigned size;
 
   lock_acquire (&filesystem_lock);
   fd = get_arg_int (esp, 1);
   size = get_arg_int (esp, 3);
   buffer = get_arg_buffer (esp, 2, size);
+  printf("fd is %d\n", fd);
+  printf("size is %d\n", size);
+  printf("buffer is %s\n", (char *)buffer);
 
   /* only handle writing to console for now */
   if (fd == STDOUT_FILENO)
@@ -217,57 +222,47 @@ sys_close (uint32_t *esp UNUSED)
 
 }
 
-/* The kernel must be very careful about doing so, because the user can pass a 
-   null pointer, a pointer to unmapped virtual memory, or a pointer to kernel 
-   virtual address space (above PHYS_BASE). All of these types of invalid 
-   pointers must be rejected without harm to the kernel or other running 
-   processes, by terminating the offending process and freeing its resources. */
-bool 
-is_valid_address (const void *vaddr)
+int
+get_arg_int (void *esp, int pos)
 {
-  if (vaddr == NULL || !is_user_vaddr (vaddr))
-    return false;
-  return pagedir_get_page (thread_current ()->pagedir, vaddr) != NULL;
+  uint32_t *arg;
+  arg = (uint32_t *)esp + pos;
+  if (!is_valid_memory (arg, sizeof(int)))
+    exit (-3);
+
+  return *(int *)arg;
+}
+
+void *
+get_arg_buffer (void *esp, int pos, int size)
+{
+  uint32_t *arg;
+
+  arg = (uint32_t *)esp + pos;
+  if (!is_valid_memory (arg, size))
+    exit (-4);
+  return *(void **)arg;
 }
 
 bool 
-is_valid_buffer (void *buffer, unsigned size)
+is_valid_memory (void *start, unsigned size)
 {
   /* depending on operation, might need to double check permissions. */
   uint8_t *cur;
   uint8_t *end;
   
-  end = (uint8_t *)buffer + size;
-  for (cur = buffer; cur < end; cur++)
+  end = (uint8_t *)start + size;
+  for (cur = start; cur < end; cur++)
   {
-    if (!is_valid_address (cur))
+    if (!is_valid_address (cur) 
+        || pagedir_get_page (thread_current ()->pagedir, cur) == NULL)
       return false;
   }
   return true;
 }
 
-void *
-get_arg_buffer (uint32_t *esp, int pos, int size)
+bool 
+is_valid_address (const void *vaddr)
 {
-  uint32_t *arg;
-  void *buffer;
-
-  arg = esp + pos;
-  if (!is_valid_address (arg))
-    exit (-1);
-  buffer = *(void **)arg;
-  if (!is_valid_buffer (buffer, size))
-    exit (-1);
-  return buffer;
-}
-
-int
-get_arg_int (uint32_t *esp, int pos)
-{
-  uint32_t *arg;
-
-  arg = esp + pos;
-  if (!is_valid_buffer (arg, sizeof(int)))
-    exit (-1);
-  return *(int *)arg;
+  return vaddr != NULL && is_user_vaddr (vaddr);
 }
