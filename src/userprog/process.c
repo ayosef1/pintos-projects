6a68c591ac8bf6cdf852c09bbceed14ad68e74fd
Abe Yosef <ayosef@myth59.stanford.edu>
2023-02-04 13:01:41 -0800

    free both pages allocated by palloc in process_execute

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -52,7 +52,10 @@ process_execute (const char *file_name)
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (token, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
-    palloc_free_page (fn_copy); 
+  {
+    palloc_free_page (fn_copy);
+    palloc_free_page (command_name); 
+  }
   
   return tid;
 }
