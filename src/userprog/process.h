74d8d145d0b6b7d33eed2ae4573e8f4819aca9af
Gordon Duncan Martinez-Piedra <martigp@myth61.stanford.edu>
2023-02-10 04:16:41 -0800

    Adding description to process_Arg

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -3,19 +3,29 @@
 
 #include "threads/thread.h"
 
-#define WORD_SIZE sizeof (void *)
+#define WORD_SIZE sizeof (void *)       /* Word size for use by stack setup. */
 
-/* Struct to be passed into start_process that contains the already
-   parsed arguments and pointer to remaining arguments */
+/* Argument passed into the thread function start_process when a child
+   thread is created in process_execute (). Process execute does initial
+   parsing so stores executable name EXEC_NAME as well as pointer SAVE_PTR
+   to rest of the command line.
+   
+   PAGE is a pointer to the temporary buffer on which command line is stored
+   so child process can free it.
+   
+   LOADED used to return load status of child back to parent and LOADED_SEMA
+   to synchronize parent return from process_execute () and child completing
+   load. */
 struct process_arg 
     {
-        char *exec_name;                /* Name of executable */
-        char *save_ptr;                 /* Save ptr from tokenizing exec */
-        char *page;                     /* Page on which cmd_line storage */
-        bool loaded;                    /* Child loaded successfully */
-        struct semaphore loaded_sema;   /* Ensure parent waits for child 
-                                           to load. */
+        char *exec_name;                /* Name of executable. */
+        char *save_ptr;                 /* Save ptr from tokenizing cmd line. */
+        char *page;                     /* Page on which cmd_line stored. */
+        bool loaded;                    /* Whether child load successful. */
+        struct semaphore loaded_sema;   /* Ensure parent waits for child to
+                                           load. */
     };
+
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
