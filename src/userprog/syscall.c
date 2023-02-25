6e726610daa6767c88e540c7664a79c20af5bcb5
Gordon Duncan Martinez-Piedra <martigp@myth61.stanford.edu>
2023-02-24 16:51:00 -0800

    Making exit non globall and chaning address validation to the second style from project 2

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -31,8 +31,6 @@ static void sys_seek (uint32_t *esp);
 static unsigned sys_tell (uint32_t *esp);
 static void sys_close (uint32_t *esp);
 
-static void exit (int status);
-
 static char *get_arg_string (void *esp, int pos, int limit);
 static void *get_arg_buffer (void *esp, int pos, int size);
 static int get_arg_int (void *esp, int pos);
@@ -277,7 +275,7 @@ sys_read (uint32_t *esp)
           return -1;
 
         lock_acquire (&filesys_lock);
-        bytes_read = file_read(fp, buffer, size);
+        bytes_read = file_read (fp, buffer, size);
         lock_release (&filesys_lock);
     }
   
@@ -482,8 +480,7 @@ is_valid_memory (void *start, unsigned size)
 static bool 
 is_valid_address (void *vaddr)
 {
-  return vaddr != NULL && is_user_vaddr (vaddr)
-                       && pagedir_get_page (thread_current ()->pagedir, vaddr);
+  return vaddr != NULL && is_user_vaddr (vaddr);
 }
 
 /* Returns whether FD is between 0 and MAX_FILES */
