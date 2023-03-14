2ae3b2170a3fa56b0db8b17d78091fdb70a48b4b
Gordon Duncan Martinez-Piedra <martigp@myth51.stanford.edu>
2023-03-14 13:17:09 -0700

    Changing access for cache to be more detailed, includes 4 different types of use

--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -4,6 +4,7 @@
 #include "devices/block.h"
 #include "devices/timer.h"
 #include "filesys/off_t.h"
+#include "threads/synch.h"
 
 /* Size of buffer cache. */
 #define CACHE_SIZE 64
@@ -12,7 +13,40 @@
 /* Number of ticks between periodic buffer cache write back. */
 #define WRITE_BACK_PERIOD TIMER_FREQ * 10
 
+enum cache_use_type
+    {
+        W_EXCL,                         /* Exclusive write. */
+        W_SHARE,                        /* Non exclusive write. */
+        R_SHARE,                        /* Read of data. */
+        R_AHEAD                         /* Read ahead. */
+    };
+
+/* An entry in the buffer cache. */
+struct cache_entry
+    {
+        block_sector_t sector;          /* Block sector represented. */
+        int write_refs;                 /* Total number of writer references. */
+        int total_refs;                 /* Total number of references. */
+        bool accessed;                  /* Accessed bit for eviction. */
+        bool dirty;                     /* Dirty bit for eviction and write
+                                           back. */
+        bool allocated;                 /* Whether cache_entry has been 
+                                           allocated. */
+        struct condition no_writers;    /* For write back, signal when 
+                                           write_refs decremented to zero. */
+        struct condition no_refs;       /* For eviction, when on*/
+        struct lock lock;               /* Lock to synchronize access to cache
+                                           entry metadata. */  
+        uint8_t *data;                  /* Actual cached sector. */
+    };
+
+struct cache_entry;
+
+
 void cache_init (void);
+struct cache_entry *cache_get_entry (block_sector_t sector,
+                                     enum cache_use_type type);
+struct cache_entry *cache_add_sector (block_sector_t sector, bool new);
 void cache_read (block_sector_t sector, void *buffer, off_t size, off_t ofs);
 void cache_write (block_sector_t sector, const void *buffer, off_t size,
                   off_t ofs);
